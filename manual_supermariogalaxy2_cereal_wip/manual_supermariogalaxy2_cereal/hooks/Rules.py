78169f049e95c17e0827b6ab169aa43bdfbcfe02
carbon-cosmic <corecarboncosmos@gmail.com>
2026-07-28 10:50:55 -0700

    Add files via upload

---
+++ manual_supermariogalaxy2_cereal_wip/manual_supermariogalaxy2_cereal/hooks/Rules.py
@@ -0,0 +1,8 @@
+from typing import Optional
+from worlds.AutoWorld import World
+from ..Helpers import clamp, get_items_with_value
+from BaseClasses import MultiWorld, CollectionState
+
+import re
+
+# You can return a string from your function, and it will be evaluated as a requires string.
